dc7532bbc03f2860e194c204380fb13ece7a2ccd
Doug Richardson <54010293+dougrichardson@users.noreply.github.com>
2024-08-29 17:24:15 +1000

    Started exporting common functions

---
+++ functions.py
@@ -0,0 +1,12 @@
+import xarray as xr
+
+def load_hourly_cf(year, gen_type, method="van_der_Wiel"):
+    """
+    Load one year of hourly generation data.
+    
+    year: int, year to load
+    gen_type: str, 'wind' or 'solar'
+    method: str, method used to compute capacity factor, e.g. 'van_der_Wiel'
+    """
+    cf_path = "/g/data/w42/dr6273/work/projects/Aus_energy/production_metrics/" + gen_type + "/capacity_factor/" + method + "/"
+    return xr.open_mfdataset(cf_path + gen_type + "_capacity_factor_" + method + "_era5_hourly_" + str(year) + "_Aus.nc")
